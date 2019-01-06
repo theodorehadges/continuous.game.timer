8c6e509e6757645846715fac28144a4e40ae5e85
Theodore Hadges <theodore.hadges@columbia.edu>
2019-01-06 14:18:18 -0500

    Delete unnecessary code

--- game_timer.py
+++ game_timer.py
@@ -8,10 +8,6 @@ from time import sleep
 TURN_TIME = 5
 TIME_BETWEEN_TURNS = 1
 
-frequency_start = 1000 # Hertz
-frequency_stop = 500 # Hertz
-duration  = 5000 # milliseconds
-
 ready_go = "./go.wav"
 buzzer_x = "./buzzer_x.wav"
 
